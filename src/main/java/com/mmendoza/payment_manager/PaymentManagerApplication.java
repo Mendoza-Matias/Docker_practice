b8e66f6e43dfc8d75e312aa15e6ab279ab2073ae
Mendoza Matias <mendoza.e.matias@gmail.com>
2026-02-24 11:38:36 -0300

    I add basic controller for test

--- src/main/java/com/mmendoza/payment_manager/PaymentManagerApplication.java
+++ src/main/java/com/mmendoza/payment_manager/PaymentManagerApplication.java
@@ -2,12 +2,22 @@ package com.mmendoza.payment_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/payments")
 public class PaymentManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentManagerApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("Hello, Payment Manager!");
+	}
 }
