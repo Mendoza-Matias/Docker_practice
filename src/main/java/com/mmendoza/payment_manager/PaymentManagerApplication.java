3a8a2f4a67c804bd719d5fa411360c9f936cfacb
Mendoza Matias <mendoza.e.matias@gmail.com>
2026-02-24 08:53:59 -0300

    first commit

---
+++ src/main/java/com/mmendoza/payment_manager/PaymentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mmendoza.payment_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmendoza/payment_manager/PaymentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmendoza.payment_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
